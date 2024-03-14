155e2177b5603240d3ec4153a9bf3a7122c7177b
Suraj Sharma <suraj200delhi@gmail.com>
2024-03-14 18:31:03 +0530

    name related

--- day_01.js
+++ day_01.js
@@ -1,4 +1,5 @@
-// Write a function createHelloWorld. It should return a new function that always returns "Hello World".
+// Write a function createHelloWorld.
+// It should return a new function that always returns "Hello World".
  
 
 // Example 1:
